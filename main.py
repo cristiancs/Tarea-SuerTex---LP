755878cf21415808cff02e5166ae21535166c942
CristiaN <cristiannavarrete@outlook.com>
2016-08-17 11:55:29 -0300

    fix numero de linea cuando esta en blanco.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import re
 from funciones import *
 archivo = open("suertex.txt", "r")
 i = 0
+i2 = 1
 n_linea = 0
 flags = {"separamiles": False,"ofecha": False,"error": 0}
 list_flag = {"inicio": False, "item": False}
@@ -21,7 +22,7 @@ for linea in archivo:
 		if i < 3:
 			flags["separamiles"] = True
 		else:
-			printError(result.group(),i, "ha sido declarada en un lugar incorrecto")
+			printError(result.group(),i2, "ha sido declarada en un lugar incorrecto")
 			flags["error"]+=1
 	# Buscar  ofecha
 	result = re.search(r'\\ofecha{}',linea)
@@ -29,20 +30,20 @@ for linea in archivo:
 		if i < 3:
 			flags["ofecha"] = True
 		else:
-			printError(result.group(),i, "ha sido declarada en un lugar incorrecto")
+			printError(result.group(),i2, "ha sido declarada en un lugar incorrecto")
 			flags["error"]+=1
 	# Buscar titulo de la página
 	result = re.search(r'\\nproy{[a-zA-Z\s]{1,}}',linea)
 	if result:
 		if data["nproy"] != False:
-			printError("\\nproy",i, "ha sido declarada más de una vez")
+			printError("\\nproy",i2, "ha sido declarada más de una vez")
 		else:
 			data["nproy"] = result.group()
 	# Buscar { mal usadas
 	result = re.findall(r'[a-zA-Z\s]{1,}{',linea)
 	for text in result:
 		if text.strip("{") not in validFunctions:
-			printError(text,i, "no es una función valida")
+			printError(text,i2, "no es una función valida")
 			flags["error"]+=1
 	# Buscar } mal usadas
 	result = re.findall(r'.{1,}}',linea)
@@ -51,7 +52,7 @@ for linea in archivo:
 		r2 = r2[0].split("\\")
 		#print r2
 		if len(r2) == 1 or r2[-1] not in validFunctions:
-			printError(r2[-1][:-1],i, "no es una función valida")
+			printError(r2[-1][:-1],i2, "no es una función valida")
 			flags["error"]+=1
 	# Buscar \ mal usadas
 	result = re.findall(r'\\.[^\s-]{0,}',linea)
@@ -59,7 +60,7 @@ for linea in archivo:
 		result = map(limpiar, result)
 		for funcion in result:
 			if funcion not in validFunctions:
-				printError("/",i, " no esta permitida fuera de las variables")
+				printError("/",i2, " no esta permitida fuera de las variables")
 				flags["error"]+=1
 	
 	# Verificar que se usen bien los { }, [ ]
@@ -69,12 +70,12 @@ for linea in archivo:
 			brackets.append(caracter)
 		if re.match(r'[\]\}]', caracter):
 			if(len(brackets) == 0):
-				printError("Hay una llave mal cerrada/abierta", i,"")
+				printError("Hay una llave mal cerrada/abierta", i2,"")
 				flags["error"]+=1
 			else:
 				brackets.pop()
 	if(len(brackets) > 0):
-		printError("Hay una llave mal cerrada/abierta", i,"")
+		printError("Hay una llave mal cerrada/abierta", i2,"")
 		flags["error"]+=1
 	#Agregar inicio / fin a una lista
 	result = re.findall(r'\\inicio{.{0,}}|\\fin{.{0,}}',linea)
@@ -83,25 +84,25 @@ for linea in archivo:
 		#Buscar que el parametro que recibe es correcto el inicio/fin
 		argmt = re.search(r'{.*}', tag)
 		if argmt.group()[1:-1] != 'lista_enumerada' and argmt.group()[1:-1] != 'lista_punteada':
-			printError(argmt.group()[1:-1], i," no es un parametro valido para el tag \inicio o \\fin")
+			printError(argmt.group()[1:-1], i2," no es un parametro valido para el tag \inicio o \\fin")
 			flags["error"]+=1 
 
 	# Verificar que los items esten dentro de un \inicio
 	result = re.findall(r'\\item',linea)
 	if result and not re.search(r'\\inicio{.{0,}}', inicioFinList[-1]):
-		printError("\item", i,"se encuentra fuera de una lista")
+		printError("\item", i2,"se encuentra fuera de una lista")
 		flags["error"]+=1 
 
 	# Verificar items dentro de inicio
 	if re.search(r'\\inicio{', linea):	# Buscar inicio de lista
 		if list_flag["inicio"]:		# Si ya habia iniciado
-			printError("\inicio", i, "hay un inicio dentro de otro inicio")
+			printError("\inicio", i2, "hay un inicio dentro de otro inicio")
 			flags["error"]+=1 
 		else:
 			list_flag["inicio"] = True
 			n_linea = i
 		if not re.search(r'}[\s]*$', linea):	# Incorrecto cierre de linea
-			printError("\inicio", i, "hay texto fuera de un item en una lista")
+			printError("\inicio", i2, "hay texto fuera de un item en una lista")
 			flags["error"]+=1 
 	if list_flag["inicio"] and i > n_linea:
 		if re.search(r'^\\item{.*}$', linea):	# Actualizar flag de item
@@ -109,13 +110,14 @@ for linea in archivo:
 				list_flag["item"] = True
 		elif re.search(r'\\fin{', linea):		# Buscar cierre y actualizar
 			if not list_flag["item"]:
-				printError("\\fin", i, "no hay items en la lista")
+				printError("\\fin", i2, "no hay items en la lista")
 				flags["error"]+=1 
 			list_flag["inicio"] = False
 		else:
 			if re.search(r'[\S]+', linea):		# Si no encontro item ni fin, buscar si hay algo demas
-				printError("\inicio", i, "hay texto fuera de un item en una lista")
+				printError("\inicio", i2, "hay texto fuera de un item en una lista")
 				flags["error"]+=1 
+	i2+=1
 if not data["nproy"]:
 	#El titulo nunca fue declarado
 	printError("\\nproy",-1, "No ha sido declarada")
