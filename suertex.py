9cfb3ff19db30c0a86f31e29f80db7616c4e527a
CristiaN <cristiannavarrete@outlook.com>
2016-08-22 13:45:35 -0300

    limitar a variables prohibidas

--- suertex.py
+++ suertex.py
@@ -63,7 +63,7 @@ for linea in archivo:
 	result = re.findall(r'\\.[^\s-]*',linea)
 	if result:
 		result = map(limpiar, result)
-		if len(result) > 1 and re.search(r'(\}|\s){1,} \\\w*', linea):
+		if len(result) > 1 and re.search(r'(inicio|nproy|titulo|inicio|item|fin){.{0,}(\}|\s){1,} \\\w*', linea):
 			printError("\\",i2, " no esta permitido el uso de 2 \  en una linea (posiblemente 2 comandos en misma linea)")
 			flags["error"]+=1
 		for funcion in result:
